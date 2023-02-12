642f65e2e965734bd16258120aa3cfab0dff9bbe
axexeyvershinin <og2001@yandex.ru>
2023-02-12 10:06:08 +0300

    в models.py добавил классы Order и PositionOrder, выполнил миграции

--- payments/models.py
+++ payments/models.py
@@ -6,8 +6,10 @@ from django.urls import reverse
 class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
-    price = models.IntegerField(verbose_name='price in cents')  # центы, т.к. stripe API принимает цену в центах
+    price = models.IntegerField(
+        verbose_name='price in cents')  # цена товара в центах, т.к. платежный сервис Stripe принимает цену в центах
 
+    # возвращает строковое представление объекта.
     def __str__(self):
         return self.name
 
@@ -15,10 +17,30 @@ class Item(models.Model):
     class Meta:
         ordering = ['name']  # сортировка в админке
 
-    # определяем метод, которым выстраиваем ссылку для подробного описания объекта и передаем ее в шаблон
+    # возвращает URL для доступа к детальной информации о товаре
     def get_absolute_url(self):
         return reverse('detail_item', kwargs={'item_id': self.pk})
 
-    # данный метод вернет цену в долларах
+    # возвращает цену товара в долларах, для удобства отображения на страницах сайта
     def get_display_price(self):
         return '{0:.2f}'.format(self.price / 100)
+
+
+class Order(models.Model):
+    date_create = models.DateTimeField(auto_now_add=True)
+    order_price = models.FloatField(null=True)
+    items = models.ManyToManyField(Item,
+                                   through='PositionOrder')  # связь многие ко многим через таблицу PositionOrder
+
+    def __str__(self):
+        return f'Order №{self.pk} dated {self.date_create.strftime("%b %d, %Y")}'
+
+    class Meta:
+        ordering = ['-pk']
+
+
+class PositionOrder(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.PROTECT)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    count_item = models.IntegerField()
+    price = models.IntegerField()
